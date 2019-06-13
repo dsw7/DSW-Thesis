054ffae34582555119cea75c4234c627469f47ec
David Weber <dsw7@sfu.ca>
2019-06-12 21:53:44 -0700

    Remove grad school routine

--- thesis_Met-aromatic_workdir_LOWLEVEL/README.txt
+++
@@ -1,4 +0,0 @@
-A lower level implementation of the Met-aromatic algorithm.
-
-The script thesis_Met-aromatic_workdir_LOWLEVEL.py depicts example usage
-of the low level implementation.
--- thesis_Met-aromatic_workdir_LOWLEVEL_2/runner.py
+++ thesis_Met-aromatic_workdir_LOWLEVEL_2/runner.py
@@ -1,30 +1,45 @@
 """
 dsw7@sfu.ca
+The "main" script that accepts PDB code input from user
 """
 
 from sys import path
 path.append(r"utils")
 from ma import MetAromatic
-from pandas import read_csv, DataFrame
-
-START = 1500
-END = 1600
+from pandas import DataFrame
+from argparse import ArgumentParser
 COLUMNS = ["ARO", "ARO POS", "MET", "MET POS", "NORM", "MET-THETA", "MET-PHI"]
 
-df = read_csv("randomized_pdb_codes.csv")
-df = df.iloc[START:END]
-df.columns = ["CODE"]
+parser = ArgumentParser(description="Input a PDB code:")
+parser.add_argument('--code', help='Usage: $ python runner.py --code <1abc>', default='1rcy')
+parser.add_argument('--cutoff', help='Usage: $ python runner.py --cutoff <float>', default=6.0, type=float)
+parser.add_argument('--angle', help='Usage: $ python runner.py --angle <float>', default=109.5, type=float)
+parser.add_argument('--model', help='Usage: $ python runner.py --model <cp|rm>', default='cp')
 
-for code in df.CODE:
-    try:
-        print("Analyzing: {}".format(code))
-        results = MetAromatic(code).met_aromatic()
-        if not results:
-            print("No interactions.")
-        else:
-            df = DataFrame(results)
-            df.columns = COLUMNS
-            print(df)
-    except Exception as exception:
-        print("An exception has occurred:")
-        print(exception)
+code = parser.parse_args().code
+cutoff = parser.parse_args().cutoff
+angle = parser.parse_args().angle
+model = parser.parse_args().model
+
+if len(code) != 4:
+    exit('Invalid pdb code: {}'.format(code))
+else:
+    pass
+
+try:
+    print("Analyzing: {}".format(code))
+    print("Cutoff: {}".format(cutoff))
+    print("Angle: {}".format(angle))
+    print("Model: {}\n".format(model))
+    results = MetAromatic(code, cutoff=cutoff, angle=angle, model=model).met_aromatic()
+    if not results:
+        print("No interactions.")
+    else:
+        df = DataFrame(results)
+        df.columns = COLUMNS
+        print(df)
+except Exception as exception:
+    print("An exception has occurred:")
+    print(exception)
+else:
+    print('-' * 65)
